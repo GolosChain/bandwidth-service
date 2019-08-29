695ccaf218d5277200c7c3667c3d16941b8e4e1e
IvanBlacky <tcherivan@gmail.com>
2019-08-29 16:45:30 +0300

    Refactor: error messages

--- src/controllers/BandwidthProvider.js
+++ src/controllers/BandwidthProvider.js
@@ -170,7 +170,7 @@ class BandwidthProvider extends BasicController {
                 serializedTransaction,
             });
         } catch (error) {
-            Logger.error('Transaction send failed:',error);
+            Logger.error('Transaction send failed:', error);
             throw error;
         }
     }
